a49964db561889fbb747636f0aae6af8c341f692
MarkQuach12 <mark.quach1234@gmail.com>
2025-12-19 12:38:32 +1100

    Added snowfall

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,17 @@ import { NavBar } from "./components/layouts/nav-bar";
 import { AboutMePage } from "./pages/AboutMePage";
 import { BlogPage } from "./pages/BlogPage";
 import { ProjectsPage } from "./pages/ProjectsPage";
+import Snowfall from 'react-snowfall'
+import { useTheme } from "next-themes";
 
 function App() {
+  const { theme } = useTheme();
+
   return (
     <>
       <div className="w-full max-w-3xl mx-auto px-4 sm:px-6 lg:px-8">
         <NavBar />
+        <Snowfall color={theme === "dark" ? "white" : "#D3D3D3"} snowflakeCount={50} style={{ position: "fixed", width: "100%", height: "100%" }} />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutMePage />} />
